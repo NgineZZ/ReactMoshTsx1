ac6e108f22c776adc568c157a3f8f749fc8dd447
Faisal Kabir <faisal.kabir71@gmail.com>
2024-05-27 03:51:47 +0600

    Adding a Expandable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-// import { Like } from "./Components/Like";
-// import Button from "./Components/Button/Butto CounterList
 
-import CounterList from "./Components/CounterList";
-import DeleteList from "./Components/DeleteList";
-
-// import { useState } from "react";
-
-
-
+import ExpandableText from "./Components/ExpandableText";
 
 function App() {
   return (
     <div>
-      <CounterList />
-      <DeleteList />
+      {/* <CounterList /> */}
+      {/* <DeleteList /> */}
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum velit est praesentium molestiae quisquam culpa. Molestiae perferendis fugiat pariatur mollitia minima, nostrum ut magnam, quos corrupti hic perspiciatis iure dolorum, beatae ipsa. Libero reiciendis voluptatibus impedit dolor debitis animi, ipsa tempore commodi, aut repellendus optio omnis, odio quae at alias officiis. Enim maiores deserunt inventore deleniti cumque? Incidunt, voluptatibus. Nesciunt harum necessitatibus illum, amet voluptate ratione iste sequi doloribus laboriosam voluptatum tempora possimus? Doloribus praesentium, eaque ipsum ab vitae ad! Vero placeat adipisci ab, molestias vel non ut animi tenetur eveniet maiores exercitationem commodi itaque atque blanditiis. Eveniet, quisquam fugit?
+      </ExpandableText>
     </div>
   );
 }
